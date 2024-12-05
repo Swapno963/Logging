456fe0b02d8b64df9ae599f79f47713ec9961213
swapno963 <swapno963@gmail.com>
2024-12-05 22:55:23 +0600

    logging is working

--- demolog/views.py
+++ demolog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+import logging
+from django.contrib.auth.models import User
 # Create your views here.
+
+
+logger = logging.getLogger(__name__) # demolog.views
+
+def index(request):
+    context = {}
+    # pass some message in logger
+    logger.info("Testing my logger")
+    try:
+        User.objects.get(pk=1)
+    except User.DoesNotExist:
+        logger.error("User with ID %s does not exist",1)
+    return render(request, "index.html", context)
